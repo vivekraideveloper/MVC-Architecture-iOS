7e5e853d3ef25f5230b3f1636e6eafce906c78eb
Vivek Rai <vivekraiiosdev@gmail.com>
2019-01-19 15:08:28 +0530

    Completed MVC Architecture

---
+++ MVC Architecture/View/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  MVC Architecture
+//
+//  Created by Vivek Rai on 19/01/19.
+//  Copyright © 2019 Vivek Rai. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
